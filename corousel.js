6819331a549bd6082ffefb5605984e61907c85c9
novitafinisher <59291957+novitafinisher@users.noreply.github.com>
2023-09-16 13:03:13 +0530

    Revert "Saurav"

--- corousel.js
+++
@@ -1,9 +0,0 @@
- // <!-- COROUSEL SCRIPT -->
- var counter = 1;
- setInterval(function(){
- document.getElementById('radio' + counter).checked = true;
- counter++;
- if(counter > 4){
-     counter = 1;
- }
- }, 3000);
